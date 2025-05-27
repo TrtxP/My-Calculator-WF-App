5953dd37cb43b97ba3f767dffd14f8cf219b00a4
Ilya <windthesa263465@outlook.com>
2025-05-27 18:08:50 +0300

    Добавил папку Calculator с проектом как обычную папку

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Calculator
+{
+    partial class FormCalculatorWFApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCalculatorWFApp));
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
+            this.buttonThree = new System.Windows.Forms.Button();
+            this.buttonFour = new System.Windows.Forms.Button();
+            this.buttonFive = new System.Windows.Forms.Button();
+            this.buttonSix = new System.Windows.Forms.Button();
+            this.buttonSeven = new System.Windows.Forms.Button();
+            this.buttonEight = new System.Windows.Forms.Button();
+            this.buttonNine = new System.Windows.Forms.Button();
+            this.buttonZero = new System.Windows.Forms.Button();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.buttonDecimalPoint = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.buttonMultiplication = new System.Windows.Forms.Button();
+            this.buttonSqrt = new System.Windows.Forms.Button();
+            this.buttonOneDivideByAnotherExpression = new System.Windows.Forms.Button();
+            this.buttonClearing = new System.Windows.Forms.Button();
+            this.buttonRemainder = new System.Windows.Forms.Button();
+            this.buttonBackspace = new System.Windows.Forms.Button();
+            this.buttonPlusMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.BackColor = System.Drawing.SystemColors.Info;
+            resources.ApplyResources(this.txtDisplay, "txtDisplay");
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.TextChanged += new System.EventHandler(this.txtDisplay_TextChanged);
+            this.txtDisplay.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDisplay_KeyDown);
+            this.txtDisplay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDisplay_KeyPress);
+            // 
+            // buttonOne
+            // 
+            resources.ApplyResources(this.buttonOne, "buttonOne");
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.UseVisualStyleBackColor = true;
+            this.buttonOne.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonTwo
+            // 
+            resources.ApplyResources(this.buttonTwo, "buttonTwo");
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.UseVisualStyleBackColor = true;
+            this.buttonTwo.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonThree
+            // 
+            resources.ApplyResources(this.buttonThree, "buttonThree");
+            this.buttonThree.Name = "buttonThree";
+            this.buttonThree.UseVisualStyleBackColor = true;
+            this.buttonThree.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonFour
+            // 
+            resources.ApplyResources(this.buttonFour, "buttonFour");
+            this.buttonFour.Name = "buttonFour";
+            this.buttonFour.UseVisualStyleBackColor = true;
+            this.buttonFour.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonFive
+            // 
+            resources.ApplyResources(this.buttonFive, "buttonFive");
+            this.buttonFive.Name = "buttonFive";
+            this.buttonFive.UseVisualStyleBackColor = true;
+            this.buttonFive.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonSix
+            // 
+            resources.ApplyResources(this.buttonSix, "buttonSix");
+            this.buttonSix.Name = "buttonSix";
+            this.buttonSix.UseVisualStyleBackColor = true;
+            this.buttonSix.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonSeven
+            // 
+            resources.ApplyResources(this.buttonSeven, "buttonSeven");
+            this.buttonSeven.Name = "buttonSeven";
+            this.buttonSeven.UseVisualStyleBackColor = true;
+            this.buttonSeven.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonEight
+            // 
+            resources.ApplyResources(this.buttonEight, "buttonEight");
+            this.buttonEight.Name = "buttonEight";
+            this.buttonEight.UseVisualStyleBackColor = true;
+            this.buttonEight.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonNine
+            // 
+            resources.ApplyResources(this.buttonNine, "buttonNine");
+            this.buttonNine.Name = "buttonNine";
+            this.buttonNine.UseVisualStyleBackColor = true;
+            this.buttonNine.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonZero
+            // 
+            resources.ApplyResources(this.buttonZero, "buttonZero");
+            this.buttonZero.Name = "buttonZero";
+            this.buttonZero.UseVisualStyleBackColor = true;
+            this.buttonZero.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonEquals
+            // 
+            resources.ApplyResources(this.buttonEquals, "buttonEquals");
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.UseVisualStyleBackColor = true;
+            this.buttonEquals.Click += new System.EventHandler(this.buttonEquals_Click);
+            // 
+            // buttonDecimalPoint
+            // 
+            resources.ApplyResources(this.buttonDecimalPoint, "buttonDecimalPoint");
+            this.buttonDecimalPoint.Name = "buttonDecimalPoint";
+            this.buttonDecimalPoint.UseVisualStyleBackColor = true;
+            this.buttonDecimalPoint.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonPlus
+            // 
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonMinus
+            // 
+            resources.ApplyResources(this.buttonMinus, "buttonMinus");
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonDivision
+            // 
+            resources.ApplyResources(this.buttonDivision, "buttonDivision");
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonMultiplication
+            // 
+            resources.ApplyResources(this.buttonMultiplication, "buttonMultiplication");
+            this.buttonMultiplication.Name = "buttonMultiplication";
+            this.buttonMultiplication.UseVisualStyleBackColor = true;
+            this.buttonMultiplication.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonSqrt
+            // 
+            resources.ApplyResources(this.buttonSqrt, "buttonSqrt");
+            this.buttonSqrt.Name = "buttonSqrt";
+            this.buttonSqrt.UseVisualStyleBackColor = true;
+            this.buttonSqrt.Click += new System.EventHandler(this.buttonSqrt_Click);
+            // 
+            // buttonOneDivideByAnotherExpression
+            // 
+            resources.ApplyResources(this.buttonOneDivideByAnotherExpression, "buttonOneDivideByAnotherExpression");
+            this.buttonOneDivideByAnotherExpression.Name = "buttonOneDivideByAnotherExpression";
+            this.buttonOneDivideByAnotherExpression.UseVisualStyleBackColor = true;
+            this.buttonOneDivideByAnotherExpression.Click += new System.EventHandler(this.buttonOneDivideByAnotherExpression_Click);
+            // 
+            // buttonClearing
+            // 
+            resources.ApplyResources(this.buttonClearing, "buttonClearing");
+            this.buttonClearing.Name = "buttonClearing";
+            this.buttonClearing.UseVisualStyleBackColor = true;
+            this.buttonClearing.Click += new System.EventHandler(this.buttonClearing_Click);
+            // 
+            // buttonRemainder
+            // 
+            resources.ApplyResources(this.buttonRemainder, "buttonRemainder");
+            this.buttonRemainder.Name = "buttonRemainder";
+            this.buttonRemainder.UseVisualStyleBackColor = true;
+            this.buttonRemainder.Click += new System.EventHandler(this.DigitButton_Click);
+            // 
+            // buttonBackspace
+            // 
+            resources.ApplyResources(this.buttonBackspace, "buttonBackspace");
+            this.buttonBackspace.Name = "buttonBackspace";
+            this.buttonBackspace.UseVisualStyleBackColor = true;
+            this.buttonBackspace.Click += new System.EventHandler(this.buttonBackspace_Click);
+            // 
+            // buttonPlusMinus
+            // 
+            resources.ApplyResources(this.buttonPlusMinus, "buttonPlusMinus");
+            this.buttonPlusMinus.Name = "buttonPlusMinus";
+            this.buttonPlusMinus.UseVisualStyleBackColor = true;
+            this.buttonPlusMinus.Click += new System.EventHandler(this.buttonPlusMinus_Click);
+            // 
+            // FormCalculatorWFApp
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.buttonPlusMinus);
+            this.Controls.Add(this.buttonBackspace);
+            this.Controls.Add(this.buttonRemainder);
+            this.Controls.Add(this.buttonClearing);
+            this.Controls.Add(this.buttonOneDivideByAnotherExpression);
+            this.Controls.Add(this.buttonSqrt);
+            this.Controls.Add(this.buttonMultiplication);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonDecimalPoint);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.buttonZero);
+            this.Controls.Add(this.buttonNine);
+            this.Controls.Add(this.buttonEight);
+            this.Controls.Add(this.buttonSeven);
+            this.Controls.Add(this.buttonSix);
+            this.Controls.Add(this.buttonFive);
+            this.Controls.Add(this.buttonFour);
+            this.Controls.Add(this.buttonThree);
+            this.Controls.Add(this.buttonTwo);
+            this.Controls.Add(this.buttonOne);
+            this.Controls.Add(this.txtDisplay);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "FormCalculatorWFApp";
+            this.Load += new System.EventHandler(this.FormCalculatorWFApp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.Button buttonTwo;
+        private System.Windows.Forms.Button buttonThree;
+        private System.Windows.Forms.Button buttonFour;
+        private System.Windows.Forms.Button buttonFive;
+        private System.Windows.Forms.Button buttonSix;
+        private System.Windows.Forms.Button buttonSeven;
+        private System.Windows.Forms.Button buttonEight;
+        private System.Windows.Forms.Button buttonNine;
+        private System.Windows.Forms.Button buttonZero;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.Button buttonDecimalPoint;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Button buttonMultiplication;
+        private System.Windows.Forms.Button buttonSqrt;
+        private System.Windows.Forms.Button buttonOneDivideByAnotherExpression;
+        private System.Windows.Forms.Button buttonClearing;
+        private System.Windows.Forms.Button buttonRemainder;
+        private System.Windows.Forms.Button buttonBackspace;
+        private System.Windows.Forms.Button buttonPlusMinus;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FormCalculatorWFApp : Form
+    {
+        public FormCalculatorWFApp()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCalculatorWFApp_Load(object sender, EventArgs e)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+            System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            txtDisplay.Text = "0";
+
+            txtDisplay.SelectionStart = txtDisplay.Text.Length;
+            txtDisplay.SelectionLength = 0;
+
+            this.ActiveControl = null;
+        }
+
+        bool criticalError = false;
+        bool isResultDisplayed = false;
+        double num;
+        double result;
+
+        private void DigitButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn == null) return;
+
+            string currentText = txtDisplay.Text;
+            string newChar = btn.Text;
+
+            if (isResultDisplayed)
+            {
+                if (char.IsDigit(newChar[0]) || newChar == ".")
+                {
+                    txtDisplay.Text = newChar == "." ? "0." : newChar;
+                    isResultDisplayed = false;
+                }
+                else
+                {
+                    isResultDisplayed = false;
+                    txtDisplay.Text += newChar;
+                }
+                return;
+            }
+
+            if (currentText == "0" && char.IsDigit(newChar[0]))
+            {
+                txtDisplay.Text = newChar;
+                return;
+            }
+
+            if ("+-*/.%".Contains(newChar))
+            {
+                if (currentText.Length > 0 && "+-*/.%".Contains(currentText[currentText.Length - 1].ToString()))
+                {
+                    return;
+                }
+            }
+
+
+            if (newChar == ".")
+            {
+                string[] parts = currentText.Split(new char[] { '+', '-', '*', '/', '%' });
+                string lastPart = parts.Length > 0 ? parts[parts.Length - 1] : "";
+
+                if (lastPart.Contains("."))
+                {
+                    return;
+                }
+            }
+
+            txtDisplay.Text += newChar;
+        }
+
+        private void buttonSqrt_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDisplay.Text, out num) && num != 0)
+            {
+                txtDisplay.Text = (Math.Sqrt(num)).ToString();
+                isResultDisplayed = true;
+            }
+        }
+
+
+        private void buttonPlusMinus_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDisplay.Text, out num))
+            {
+                num = -num;
+                txtDisplay.Text = num.ToString();
+                isResultDisplayed = true;
+            }
+        }
+
+        private void buttonOneDivideByAnotherExpression_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtDisplay.Text, out num))
+            {
+                if (num == 0)
+                {
+                    txtDisplay.Text = "Error: Division by Zero";
+                    criticalError = true;
+                    return;
+                }
+
+                double result = 1 / num;
+
+                if (double.IsInfinity(result) || double.IsNaN(result))
+                {
+                    txtDisplay.Text = "Error: Division by Zero";
+                    criticalError = true;
+                    return;
+                }
+
+                txtDisplay.Text = result.ToString();
+                criticalError = false;
+                isResultDisplayed = true;
+            }
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            criticalError = false;
+
+            if (Regex.IsMatch(txtDisplay.Text, @"/\s*0+(\.0+)?(\D|$)") 
+                || txtDisplay.Text.EndsWith("/0") 
+                || txtDisplay.Text.Contains("/0") && !txtDisplay.Text.Contains("/0."))
+            {
+                txtDisplay.Text = "Error: Division by Zero";
+                criticalError = true;
+                return;
+            }
+
+            CalculateExpression(txtDisplay.Text);
+
+            if (criticalError || txtDisplay.Text == "" || txtDisplay.Text == "Error: Division by Zero")
+            {
+                return;
+            }
+
+            isResultDisplayed = true;
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == result.ToString())
+            {
+                return;
+            }
+
+            if (txtDisplay.Text.Length <= 1)
+            {
+                txtDisplay.Text = "0";
+            }
+            else
+            {
+                txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+            }
+        }
+
+        private void buttonClearing_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            txtDisplay.Text = "0";
+        }
+
+        private void CalculateExpression(string expression)
+        {
+            try
+            {
+                var result = new System.Data.DataTable().Compute(expression, null);
+                txtDisplay.Text = result.ToString();
+            }
+            catch (Exception)
+            {
+                txtDisplay.Text = "Error: Invalid Expression";
+                criticalError = true;
+            }
+        }
+
+        private void txtDisplay_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text.StartsWith("Error")) return;
+
+            int cursorPos = txtDisplay.SelectionStart;
+
+            string cleanedText = Regex.Replace(txtDisplay.Text, @"[^0-9+\-*/.%]", "");
+
+            if (txtDisplay.Text != cleanedText)
+            {
+                txtDisplay.Text = cleanedText;
+                txtDisplay.SelectionStart = Math.Min(cursorPos, cleanedText.Length);
+                txtDisplay.SelectionLength = 0;
+            }
+        }
+
+        private void txtDisplay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                criticalError = false;
+
+                if (Regex.IsMatch(txtDisplay.Text, @"/\s*0+(\.0+)?(\D|$)"))
+                {
+                    txtDisplay.Text = "Error: Division by Zero";
+                    criticalError = true;
+                    return;
+                }
+
+                CalculateExpression(txtDisplay.Text);
+
+                if (!criticalError && txtDisplay.Text != "" && txtDisplay.Text != "Error: Division by Zero")
+                {
+                    isResultDisplayed = true;
+                }
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+
+            if (e.KeyCode == Keys.Back)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                string currentText = txtDisplay.Text;
+
+                if (currentText == "0" || currentText.StartsWith("Error"))
+                {
+                    return;
+                }
+
+                if (currentText.Length <= 1)
+                {
+                    txtDisplay.Text = "0";
+                }
+                else
+                {
+                    txtDisplay.Text = currentText.Substring(0, currentText.Length - 1);
+                }
+            }
+        }
+
+        private void txtDisplay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char inputChar = e.KeyChar;
+            string currentText = txtDisplay.Text;
+
+            string allowedChars = "0123456789+-*/.%\b";
+
+            if (!allowedChars.Contains(inputChar.ToString()))
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (isResultDisplayed && char.IsDigit(inputChar))
+            {
+                txtDisplay.Text = inputChar.ToString();
+                e.Handled = true;
+                isResultDisplayed = false;
+                return;
+            }
+
+            if (char.IsDigit(inputChar))
+            {
+                if (currentText == "0" && !currentText.Contains("."))
+                {
+                    txtDisplay.Text = inputChar.ToString();
+                    e.Handled = true;
+                }
+
+                return;
+            }
+
+            if ("+-*/.%".Contains(inputChar.ToString()))
+            {
+                if (currentText.Length > 0 && "+-*/.%".Contains(currentText[currentText.Length - 1].ToString()))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
+
+
+            if (inputChar == '.')
+            {
+                string[] parts = currentText.Split(new char[] { '+', '-', '*', '/', '%' });
+                string lastPart = parts.Length > 0 ? parts[parts.Length - 1] : "";
+                if (lastPart.Contains("."))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
+        }
+    }
+}
